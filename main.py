3280ddf7baa149f5daf6ef31cae2ce6ecc1a1257
gallantgoat <brokennotionpa@gmail.com>
2024-11-25 22:02:35 -0500

    added constants

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import pygame
+from constants import *
 
 def main():
     print("Starting asteroids!")
 
 if __name__ == "__main__":
     main()
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
