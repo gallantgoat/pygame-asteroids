03fe620d0b168829bde0329547ec25e490972ed3
gallantgoat <brokennotionpa@gmail.com>
2024-11-27 20:19:36 -0500

    pygame display

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 from constants import *
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 def main():
+    pygame.init()
     print("Starting asteroids!")
 
 if __name__ == "__main__":
     main()
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    
+    while True:
+        screen.fill(000)
+        pygame.display.flip()
+
