00a61dcfa98cde72cc05a423db09252874a74496
gallantgoat <brokennotionpa@gmail.com>
2024-12-04 17:59:39 -0500

    added groups and looped

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     Clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     # we're making an infinite loop here for the game loop
     while True:
@@ -15,10 +18,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
-
+        for object in updatable:
+            object.update(dt)
         screen.fill(000)
-        player.draw(screen)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         # We're calling tick here at the end of each iteration of the game loop
         dt = Clock.tick(60) / 1000
