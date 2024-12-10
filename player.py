92c877e4d0ae190ad367bf1d07c3d4d2e820e4cf
gallantgoat <brokennotionpa@gmail.com>
2024-12-09 19:15:43 -0500

    shooting cooldown

--- player.py
+++ player.py
@@ -7,12 +7,14 @@ class Player(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.timer = 0
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -23,7 +25,11 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer > 0:
+                return
+            else:
+                self.shoot()
+                self.timer = PLAYER_SHOOT_COOLDOWN
 
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
